334266c552a991595e72c2f07cdde76183d79e27
Eric Marx <emarx@autoloop.com>
2021-03-20 14:02:01 -0400

    Cleaning.

--- Xramcire.Dokumentieren.Web/Controllers/DocumentsController.cs
+++ Xramcire.Dokumentieren.Web/Controllers/DocumentsController.cs
@@ -85,28 +85,3 @@ namespace Xramcire.Dokumentieren.Controllers
         }
     }
 }
-
-//Senior Software Engineer Project
-//We ask interested candidates to implement a document upload service.
-//The project is an important part of our recruiting process, and it’s designed to give candidates the
-//ability to showcase their skills in an extended time, low-stress environment.
-//We’re interested in how you write organized, structured, tested code. There is no minimum or
-//maximum project size. We’re not necessarily looking at the total quantity of functionality but rather the
-//quality, style, and characteristics.
-//Project Requirements:
-// Build a document management service API using .NET 5
-//o Supports CRUD operations
-// Write a README in your project that explains:
-//o What you built and the architecture decisions you made
-//o Thoughts on testability, scalability, and future improvements you’d make
-// Make the project available on GitHub or another Git service
-//Project Constraints:
-// Follow standard REST principles
-// Include unit tests and integration tests
-// Does not need authentication/authorization
-// Should be thread-safe and handle multiple concurrent requests
-//Final Thoughts:
-// The code you write is owned by you – feel free to make it part of your online portfolio
-// Please do not reference Free Market Music in your project as we don’t want others plagiarizing
-//your work
-// Once completed, let us know the URL of the Git repository
